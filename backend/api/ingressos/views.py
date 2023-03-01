4e9a310e4f09bb224f36456f91ba86623d88f669
alcides07 <alcidesdantasdj@gmail.com>
2023-03-01 16:01:12 -0300

    Realizando venda de ingressos

--- backend/api/ingressos/views.py
+++ backend/api/ingressos/views.py
@@ -1,10 +1,11 @@
 from django.http import request
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status, permissions
+from rest_framework import viewsets, status#, permissions
 from rest_framework.response import Response
-from rest_framework.decorators import action
-from rest_framework.authtoken.models import Token
+from django.utils import timezone
+# from rest_framework.decorators import action
+# from rest_framework.authtoken.models import Token
 from .models import Cliente, Organizador, Evento, Categoria, CompraIngresso
 from .serializers import ClienteSerializer, OrganizadorSerializer, EventoSerializer, CategoriaSerializer, VendaSerializer
 
@@ -28,8 +29,22 @@ class VendaViewSet(viewsets.ModelViewSet):
     queryset = CompraIngresso.objects.all()
     serializer_class = VendaSerializer
 
-    # @action(detail=False, methods=['get'], url_path='', permission_classes=[])
-    # def resposta(self, request, pk=None):
-    #     queryset = Cliente.objects.all()
-    #     serializer = ClienteSerializer(queryset, many=True)
-    #     return Response(serializer.data)
+    def create(self, request, pk=None):  
+        qtdIngresso = request.data["qtdIngresso"]
+        evento = get_object_or_404(Evento, id = request.data["evento"])
+
+        if (qtdIngresso <= evento.ingressoDisponivel):
+            venda = CompraIngresso.objects.create(
+                qtdIngresso = qtdIngresso,
+                valorTotal = qtdIngresso * evento.valorIngresso,
+                dataCompra = timezone.now(),
+                cliente = get_object_or_404(Cliente, id = request.data["cliente"]),
+                evento = evento
+            )
+            
+            evento.ingressoDisponivel -= venda.qtdIngresso
+            evento.save()
+            serializer = VendaSerializer(venda)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response({"Erro" : "Ingressos insuficientes"}, status=status.HTTP_400_BAD_REQUEST)
