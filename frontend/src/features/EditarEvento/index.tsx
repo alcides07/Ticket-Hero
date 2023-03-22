ee7acd854de8348fd2f234b2f0d890a1bfdc67a5
alcides07 <alcidesdantasdj@gmail.com>
2023-03-22 20:20:42 -0300

    Tela e serviço editar evento. (#11)

---
+++ frontend/src/features/EditarEvento/index.tsx
@@ -0,0 +1,16 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Navbar from "../../components/Navbar";
+import { editarEvento } from "./services/editarEvento";
+import EventoForm from "../../components/EventoForm";
+
+export default function EditarEvento(){
+    return (
+        <>
+            <Navbar/>
+            <Header/>
+            <EventoForm textoBotao = "Salvar evento" handle={editarEvento} />
+            <Footer/>
+        </>
+    );
+}
