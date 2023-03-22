d0c5e9f4e068b4f2363266874dfd6830b667e9c1
alcides07 <alcidesdantasdj@gmail.com>
2023-03-22 20:12:54 -0300

    Rotas criar e editar evento. (#11)

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,6 +4,8 @@ import Register from './features/Register';
 import Login from './features/Login';
 import Home from './features/Home';
 import CompraIngresso from './features/CompraIngresso';
+import CriarEvento from './features/CriarEvento';
+import EditarEvento from './features/EditarEvento';
 
 const Rotas = () => {
     return (
@@ -13,6 +15,8 @@ const Rotas = () => {
                 <Route path="/register" element={<Register />} />
                 <Route path="/home" element={<Home/>} />
                 <Route path="/evento/:id/compra" element={<CompraIngresso/>} />
+                <Route path="/evento/criar" element={<CriarEvento/>} />
+                <Route path="/evento/:id/editar" element={<EditarEvento/>} />
             </Routes>
         </BrowserRouter>
     );
