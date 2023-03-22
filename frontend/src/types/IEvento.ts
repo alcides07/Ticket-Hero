213644bed801e2c12de5fde3d6b505e36ea1a70a
alcides07 <alcidesdantasdj@gmail.com>
2023-03-21 23:02:39 -0300

    Organizando interfaces.

--- frontend/src/types/IEvento.ts
+++ frontend/src/types/IEvento.ts
@@ -3,6 +3,7 @@ export interface IEvento{
     categoria: string;
     nome: string;
     descricao: string;
+    nomeOrganizador: string;
     data: Date;
     imagem: string;
     valorIngresso: number;
