c3f54c3af20085dfdbc36cd2f24d07e16d4e318b
alcides07 <alcidesdantasdj@gmail.com>
2023-03-22 20:13:53 -0300

    Ajustando interfaces. (#11)

---
+++ frontend/src/types/IBotaoSubmitForm.ts
@@ -0,0 +1,11 @@
+export interface IBotaoSubmitForm {
+    textoBotao?: string;
+    mt?: string;
+    mb?: string;
+    ml?: string;
+    mr?: string;
+    pt?:string;
+    pb?:string;
+    pl?:string;
+    pr?:string;   
+}
