d1afabcaed52fd177d5cf5793cb421ec97c1dc2a
Alcides Tiago Medeiros Dantas <alcides.tiago@academico.ifrn.edu.br>
2023-04-12 11:32:02 -0300

    Usando proteção de rotas. (#51)

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import Register from './features/Register';
 import Login from './features/Login';
 import Home from './features/Home';
@@ -10,25 +9,30 @@ import VisualizarEvento from './features/VisualizarEvento';
 import MeusIngressos from './features/MeusIngressos';
 import MeusEventos from './features/MeusEventos';
 import EventosPopulares from './features/EventosPopulares';
+import PageNotFound from './components/PageNotFound';
+import { withProtectedRoute } from './components/RouteProtection';
 
 const Rotas = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={withProtectedRoute({ tipoUsuario: 'any', componente: Login })} />
+                <Route path="/home" element={withProtectedRoute({ tipoUsuario: 'any', componente: Home })} />
+                <Route path="/evento/:id/compra" element={withProtectedRoute({ tipoUsuario: 'cliente', componente: CompraIngresso })} />
+                <Route path="/eventosPopulares" element={withProtectedRoute({ tipoUsuario: 'duo', componente: EventosPopulares })} />
+                
+                {/* <Route path="/eventosPopulares" element={<EventosPopulares/>} /> */}
+                {/* <Route path="/evento/:id/compra" element={<CompraIngresso/>} /> */}
                 <Route path="/registro" element={<Register />} />
-                <Route path="/home" element={<Home/>} />
                 <Route path="/evento/:id/" element={<VisualizarEvento/>} />
-                <Route path="/evento/:id/compra" element={<CompraIngresso/>} />
                 <Route path="/evento/criar" element={<CriarEvento/>} />
                 <Route path="/evento/:id/editar" element={<EditarEvento/>} />
                 <Route path="/meusIngressos" element={<MeusIngressos/>} />
                 <Route path="/meusEventos" element={<MeusEventos/>} />
-                <Route path="/eventosPopulares" element={<EventosPopulares/>} />
+                <Route path="*" element={<PageNotFound/>} />
             </Routes>
         </BrowserRouter>
     );
   };
-  
 
 export default Rotas;
