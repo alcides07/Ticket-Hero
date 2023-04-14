6d1e508196a9f91cf0e6526dff69127b2fdd3193
Alcides Tiago Medeiros Dantas <alcides.tiago@academico.ifrn.edu.br>
2023-04-14 08:48:06 -0300

    Feedback inputs e visual dos botões de submit. (#60)

--- frontend/src/components/BotaoSubmitForm/styles.ts
+++ frontend/src/components/BotaoSubmitForm/styles.ts
@@ -13,9 +13,10 @@ export const BotaoForm = styled.button<IBotaoSubmitForm>`
     padding: 0.5em;
     border-radius: 20px;
     width: 20%;
-    background-color: #ff914d;
+    color: white;
+    background-color: #FA9C62;
     
     &:hover{
-        background-color: #FA9C62;
+        background-color: #ff914d;
     }
 `
