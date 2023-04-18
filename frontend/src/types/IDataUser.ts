9c40b8e329698b800292f340f3122104e3cf4b39
Alcides Tiago Medeiros Dantas <alcides.tiago@academico.ifrn.edu.br>
2023-04-18 11:30:16 -0300

    Criando interface para dados de usuário. (#57)

---
+++ frontend/src/types/IDataUser.ts
@@ -0,0 +1,10 @@
+export interface IDataUser{
+    rg?: string,
+    cpf?: string,
+    id: string,
+    username: string,
+    tipoUsuario: string,
+    nomeCompleto: string,
+    nascimento: string,
+    email: string
+}
